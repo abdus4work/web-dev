869b8dde4677973071ed1e0414e89a07666297e6
Abdus Sadik Molla <sadik.only4work@gmail.com>
2023-10-12 23:25:07 +0530

    initial commit

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -8,21 +8,25 @@ let pl='p1';
 box.forEach((e)=>{
     e.addEventListener('click',()=>{
         e.classList.add('disabled')
+        e.style.color="#12C7E6"
         if(pl=='p1'){
-            e.innerHTML='<i class="fa-solid fa-x"></i>';
+            e.innerText='X';
             pl='p2';
             player2.classList.add('animation')
             player1.classList.remove('animation')
             turn1.innerText="";
             turn2.innerText="Your Turn"
+            checkWin();
         }
         else{
-            e.innerHTML='<i class="fa-regular fa-circle"></i>';
+            e.style.color="#A0E612"
+            e.innerHTML='O';
             pl='p1';
             player1.classList.add('animation')
             player2.classList.remove('animation')
             turn2.innerText="";
             turn1.innerText="Your Turn"
+            checkWin();
         }
         // if(pl=='p1'){
             
@@ -32,3 +36,26 @@ box.forEach((e)=>{
         // }
     })
 })
+
+function checkWin(){
+    const win=[
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [2,4,6],
+        [0,4,8]
+    ]
+    win.forEach((e)=>{
+        if((box[e[0]].innerText==box[e[1]].innerText) && (box[e[1]].innerText==box[e[2]].innerText) && (box[e[0]].innerText !="")){
+            document.getElementById('win').removeAttribute('hidden')
+            if(box[e[0]].innerText=='X'){
+                console.log("Player 1 win......");
+            }else{
+                console.log("Player 2 win....");
+            }
+        }
+    })
+}
